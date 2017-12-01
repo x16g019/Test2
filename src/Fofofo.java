fa7b82ea647dada57fefe541a4ff6c60f555997e
WIN_NT\x16g019 <x16g019@chiba-fjb.ac.jp>
2017-12-01 10:06:57 +0900

    cc

--- src/Fofofo.java
+++ src/Fofofo.java
@@ -4,7 +4,7 @@ public class Fofofo {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 System.out.println("gggg");
-System.out.println("ggg;nklkjd");
+System.out.println("ggg;jd");
 	}
 
 }
