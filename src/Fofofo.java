70a213b7bb4b3c60b46b4be53533e76b69973960
WIN_NT\x16g019 <x16g019@chiba-fjb.ac.jp>
2017-12-01 10:41:26 +0900

    リモート

--- src/Fofofo.java
+++ src/Fofofo.java
@@ -4,11 +4,11 @@ public class Fofofo {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 System.out.println("gggg");
-<<<<<<< .merge_file_a07824
-System.out.println("ggg;nklkk,fjd");
-=======
+
+System.out.println("gglkk,fjd");
+
 System.out.println("ggg;jd");
->>>>>>> .merge_file_a03512
+
 	}
 
 }
